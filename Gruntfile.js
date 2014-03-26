5020352ff50d92d176cf4dbe07d53dda4792ea91
Ian Rose <ian@ianrose.co>
2014-03-25 21:41:36 -0400

    added favicon to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -253,10 +253,10 @@ module.exports = function (grunt) {
             'img/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             //'_bower_components/jquery/jquery.js',
-            //'favicon.ico',
+            'favicon.ico'
             //'apple-touch*.png'
           ],
           dest: '<%= yeoman.dist %>'
