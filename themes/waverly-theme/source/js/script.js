e933de41c02988ba5536da3519f9594c2db013c8
Ian Rose <ian@ianrose.me>
2015-07-20 08:50:08 -0400

    toogle menu btn, gutter mobile home, art leads work entries

--- themes/waverly-theme/source/js/script.js
+++ themes/waverly-theme/source/js/script.js
@@ -1,18 +1,23 @@
-function toggleClass(element, className){
-	if (!element || !className){
-		return;
+window.onload = init();
+
+function init() {
+	function toggleClass(element, className) {
+		if (!element || !className) {
+			return;
+		}
+
+		var classString = element.className, nameIndex = classString.indexOf(className);
+		if (nameIndex == -1) {
+			classString += ' ' + className;
+		}
+		else {
+			classString = classString.substr(0, nameIndex) + classString.substr(nameIndex + className.length);
+		}
+		element.className = classString;
 	}
 
-	var classString = element.className, nameIndex = classString.indexOf(className);
-	if (nameIndex == -1) {
-		classString += ' ' + className;
-	}
-	else {
-		classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-	}
-	element.className = classString;
+	document.getElementById('js-toggle-menu').addEventListener('click', function () {
+		toggleClass(document.getElementById('js-menu'), 'is-open');
+		toggleClass(document.getElementById('js-toggle-menu'), 'is-active');
+	});
 }
-
-document.getElementById('js-toggle-menu').addEventListener('click', function() {
-	toggleClass(document.getElementById('js-menu'), 'is-open');
-});
