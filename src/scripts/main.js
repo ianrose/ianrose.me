b05f1befd6c984cdb46bc9d921d28bf0dbb511af
Ian Rose <ian@ianrose.me>
2016-10-03 10:46:44 -0400

    filters list headings

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,19 @@
-$(document).ready(function(){
+/* global $, location, jQuery */
+$(document).ready(function () {
   'use strict';
-  var touchEventName =  ('ontouchstart' in window) ? 'touchstart':'click';
-  $('#js-toggle-menu').on(touchEventName, function() {
-   $('#js-menu').toggleClass('is-open');
-   $(this).toggleClass('is-active');
- });
+  var touchEventName = ('ontouchstart' in window) ? 'touchstart' : 'click';
+  $('#js-toggle-menu').on(touchEventName, function () {
+    $('#js-menu').toggleClass('is-open');
+    $(this).toggleClass('is-active');
+  });
 
   var scrollToOffset = 0;
 
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+  $(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
         var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
           $('html,body').animate({
             scrollTop: (target.offset().top + scrollToOffset)
@@ -23,13 +24,6 @@ $(document).ready(function(){
     });
   });
 
-  // var postList = new List('posts', {
-  //   valueNames: ['js-entry-heading-link', 'dek'],
-  //   plugins: [ ListFuzzySearch() ]
-  // });
-  //
-  // postList;
-
   /*
    * jQuery.liveFilter
    *
@@ -39,48 +33,61 @@ $(document).ready(function(){
    *
    */
 
-  (function($){
-  	$.fn.liveFilter = function(inputEl, filterEl, options){
-  		var defaults = {
-  			filterChildSelector: null,
-  			filter: function(el, val){
-  				return $(el).text().toUpperCase().indexOf(val.toUpperCase()) >= 0;
-  			},
-  			before: function(){},
-  			after: function(){}
-  		};
-  		var options = $.extend(defaults, options);
+  (function ($) {
+    $.fn.liveFilter = function (inputEl, filterEl, options) {
+      var defaults = {
+        filterChildSelector: null,
+        filter: function (el, val) {
+          return $(el).text().toUpperCase().indexOf(val.toUpperCase()) >= 0;
+        },
+        before: function () {},
+        after: function () {}
+      };
+      var options = $.extend(defaults, options);
 
-  		var el = $(this).find(filterEl);
-  		if (options.filterChildSelector) el = el.find(options.filterChildSelector);
+      var el = $(this).find(filterEl);
+      if (options.filterChildSelector) el = el.find(options.filterChildSelector);
 
-  		var filter = options.filter;
-  		$(inputEl).keyup(function(){
-  			var val = $(this).val();
-  			var contains = el.filter(function(){
-  				return filter(this, val);
-  			});
-  			var containsNot = el.not(contains);
-  			if (options.filterChildSelector){
-  				contains = contains.parents(filterEl);
-  				containsNot = containsNot.parents(filterEl).hide();
-  			}
+      var filter = options.filter;
+      $(inputEl).keyup(function () {
+        var val = $(this).val();
+        var contains = el.filter(function () {
+          return filter(this, val);
+        });
+        var containsNot = el.not(contains);
+        if (options.filterChildSelector) {
+          contains = contains.parents(filterEl);
+          containsNot = containsNot.parents(filterEl).hide();
+        }
 
-  			options.before.call(this, contains, containsNot);
+        options.before.call(this, contains, containsNot);
 
-  			contains.show();
-  			containsNot.hide();
+        contains.show();
+        containsNot.hide();
 
-  			if (val === '') {
-  				contains.show();
-  				containsNot.show();
-  			}
+        if (val === '') {
+          contains.show();
+          containsNot.show();
+        }
 
-  			options.after.call(this, contains, containsNot);
-  		});
-  	}
+        options.after.call(this, contains, containsNot);
+      });
+    };
   })(jQuery);
 
-  $('#search-list').liveFilter('.fuzzy-search', 'li', {});
+  var $listHeader = $('.list > h2');
+  var $filteredList = $listHeader.next('ul');
 
+  $('#search-list').liveFilter('.fuzzy-search', 'li', {
+    after: function () {
+      $filteredList.each(function (index, element) {
+        var filteredListItems = $(element).children(':visible').length;
+        if (filteredListItems > 0) {
+          $(element).prev('h2').show();
+        } else {
+          $(element).prev('h2').hide();
+        }
+      });
+    }
+  });
 });
