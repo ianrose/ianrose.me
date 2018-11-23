02cafb033ea9f8ee44737d833253552be182ed35
Ian Rose <ian@ianrose.me>
2018-11-23 16:57:05 -0500

    update jquery

--- src/scripts/modules/anchor-scroll.js
+++ src/scripts/modules/anchor-scroll.js
@@ -1,7 +1,7 @@
 /* globals location */
 module.exports = function (scrollToOffset = 0) {
   $(function () {
-    $('a[href*=#]:not([href=#])').click(function () {
+    $('a[href*=\\#]:not([href=\\#])').click(function () {
       if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
         var target = $(this.hash)
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
