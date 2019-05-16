6f76fd046900e1b711b90256e29ad090b985d84d
Ian Rose <ian@ianrose.me>
2019-05-16 00:49:15 -0400

    js on ie11 (#14)
    * comment out ga autotrack
    
    * adds browserslist to babel config
    
    * webpack config to include transpile on autotrack

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
       rules: [
         {
           test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules\/(?!(autotrack|dom-utils))/,
           use: [{
             loader: 'babel-loader',
             options: {
