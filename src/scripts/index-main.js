94fb74081627de8ee6bc4c9a0c3a5e05e7d17230
Ian Rose <ian@ianrose.me>
2019-05-16 00:21:47 -0400

    adds browserslist to babel config

--- src/scripts/index-main.js
+++ src/scripts/index-main.js
@@ -2,8 +2,8 @@ import $ from 'jquery'
 import 'lazysizes'
 import menu from './modules/menu'
 import anchorScroll from './modules/anchor-scroll'
-// import 'autotrack/lib/plugins/event-tracker'
-// import 'autotrack/lib/plugins/outbound-link-tracker'
+import 'autotrack/lib/plugins/event-tracker'
+import 'autotrack/lib/plugins/outbound-link-tracker'
 
 window.jQuery = $
 window.$ = $
