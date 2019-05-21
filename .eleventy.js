5357b6e7511cfd4ee1bfcbdaa799374bc9374fd2
Ian Rose <ian@ianrose.me>
2019-05-20 22:55:36 -0400

    home sections and initial collection pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
+const util = require('util')
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images')
   eleventyConfig.addPassthroughCopy('src/fonts')
+
+  eleventyConfig.addFilter('dumpCircular', function(obj) {
+    return util.inspect(obj, {showHidden: true, depth: 2, colors: true})
+  })
 
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
