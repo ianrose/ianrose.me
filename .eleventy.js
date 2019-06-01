ec1900c514b18e25dcbe6899c63d8b0034ba9a4a
Ian Rose <ian@ianrose.me>
2019-06-01 18:31:51 -0400

    init netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -9,11 +9,12 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 module.exports = function(eleventyConfig) {
 
-  // Pass Through Copt
+  // Pass Through Copy
   eleventyConfig.addPassthroughCopy('src/images')
   eleventyConfig.addPassthroughCopy('src/fonts')
   eleventyConfig.addPassthroughCopy('src/data')
   eleventyConfig.addPassthroughCopy('src/scripts/service-worker.js')
+  eleventyConfig.addPassthroughCopy('src/admin')
 
   // Filters
   eleventyConfig.addFilter('dumpCircular', function(val) {
