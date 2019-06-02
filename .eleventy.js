997c25c704ec159a788ca5a0787ce1bffa02c409
Ian Rose <ian@ianrose.me>
2019-06-02 16:08:25 -0400

    setup update for resources layout prop

--- .eleventy.js
+++ .eleventy.js
@@ -75,6 +75,7 @@ module.exports = function(eleventyConfig) {
 
   // Layout Alias
   eleventyConfig.addLayoutAlias('list.hbs', 'layouts/list.njk')
+  eleventyConfig.addLayoutAlias('list', 'layouts/list.njk')
   eleventyConfig.addLayoutAlias('post.njk', 'layouts/post.njk')
 
   // Shortcodes
